abda4bd8d615b346ef405244fdd70dbe260c1efa
Danilo <kimhodori@gmail.com>
2018-02-21 00:28:33 -0800

    adding scrapper - not working yet

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,4 +1,31 @@
-// require request and cheerio
+// require request and cheerio to make screpes possible
 
 const request = require("request");
 const cheerio = require("cheerio");
+
+const scrape = function (cb) {
+  request("http://www.bbc.com/", function(err, res, body){
+        const $ = cheerio.load(body);
+        const articles = [];
+        $(".article-info").each(function(i, element){
+
+          let head = $(this).children(".article-title").text().trim();
+          let sum = $(this).children(".excerpt").text().trim();
+          // source bookmarked on chrome tsk
+          if (head && sum) {
+            let headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+            let sumneat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+            let dataToAdd ={
+              headline: headNeat,
+              summary: sumneat
+            };
+
+            articles.push(dataToAdd);
+          }
+        });
+        cb(articles);
+      });
+};
+
+module.exports = scrape;
