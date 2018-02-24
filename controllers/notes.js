dac03405a815f15afd7c2841f210adff1dbf63ca
Danilo <kimhodori@gmail.com>
2018-02-23 20:31:21 -0800

    fixing bugs on notes.js

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,12 +1,28 @@
 // controller for notes
 
-const Note = require("../model.Note");
-const createDate = require("../script/date");
+const Note = require("../models.Note");
+let createDate = require("../scripts/date");
 
 module.exports = {
   get: function(data, cd) {
     Note.find({
       _headlineId: data._id
     }, cb);
+  },
+  save: function(data,cb) {
+    let newNote = {
+      _headlineId: data._id,
+      data: createDate(),
+      noteText: data.noteText
+    };
+    Note.create(newNOte, function (err, doc) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(doc);
+        cb(doc);
+      }
+    });
   },
 }
